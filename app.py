34b58962713140ebbedd5050b38aaf24ee402904
Senkathirselvan <senkathirsmart@gmail.com>
2024-12-11 17:24:12 +0530

    add debug mode

--- app.py
+++ app.py
@@ -129,4 +129,7 @@ def get_quote(category):
         error_message = str(e)  
         error_code = e.args
         return jsonify({"error": error_message, "code": error_code}) 
-    
+
+
+if __name__ =="__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
